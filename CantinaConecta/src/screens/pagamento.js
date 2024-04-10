376235da0b5cf3169d2797882db8341db6788a91
Catherine Markert <catmarkert@gmail.com>
2024-04-09 21:21:41 -0300

    criando tela

---
+++ CantinaConecta/src/screens/pagamento.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, Alert } from 'react-native';
+import { stylesPagamento } from './stylesPagamento';
+
+// import { Container } from './styles';
+
+const Pagamento = () => {
+  return <View />;
+}
+
+export default Pagamento;
