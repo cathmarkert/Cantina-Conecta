0e7269509b3b3be9141dfd1b4a631adb93135ffb
Catherine Markert <catmarkert@gmail.com>
2024-08-06 00:13:29 -0300

    adicionando homescreen

--- CantinaConecta/App.js
+++ CantinaConecta/App.js
@@ -7,6 +7,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import PaymentScreen from './src/screens/pagamento';
+import SelectScreen from './src/screens/selecionar';
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -52,7 +53,7 @@ function HomeTabs() {
     >
       <Tab.Screen 
         name="Selecionar" 
-        component={Home}  
+        component={SelectScreen}  
         options={{ 
           tabBarLabel: 'Selecionar',
           headerShown: false, 
